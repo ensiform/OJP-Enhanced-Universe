bc5e42087e699af243386270242196b23abb7c85
Ensiform <ensiform@gmail.com>
2024-05-26 12:25:17 -0500

    Try adding linux x86

---
+++ source/cmake/linux-x86.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+#set(CMAKE_ASM_COMPILER_ARG1 -m32)
+set(CMAKE_C_COMPILER_ARG1 -m32)
+set(CMAKE_CXX_COMPILER_ARG1 -m32)
+message(STATUS "Building with 32-bit cross compiler")
